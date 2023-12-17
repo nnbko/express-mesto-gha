70ab39593cd0b4ad3c20203c245d08690ed21664
nnb <dadaya999@yandex.ru>
2023-12-17 14:46:52 +0300

    pr14 test4

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,7 @@
-module.exports = (err, res, next) => {
-  const statusCode = err.statusCode
-  res
-    .status(statusCode)
-    .send({
-      message:
-        statusCode === 500
-          ? "На сервере ошибка"
-          : err.message,
-    });
+module.exports = (err, req, res, next) => {
+  const  statusCode = 500;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : err.message,
+  });
   next();
 };
